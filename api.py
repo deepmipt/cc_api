5a8dec7b23c67d6cc888087d3a85c06e8ef293fe
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2017-09-27 14:18:26 +0300

    feat: Add session support

--- api.py
+++ api.py
@@ -17,21 +17,48 @@ def index():
 @app.route('/answer', methods=['GET'])
 def answer():
     """
-    ChitChat
+    Generate ChitChat answer to user phrase for session
     ---
     parameters:
       - name: q
         in: query
         required: true
         type: string
+      - name: session
+        in: query
+        required: true
+        type: string
     """
     q = request.args.get('q')
+    id = request.args.get('session')
     with ClusterRpcProxy({'AMQP_URI': os.environ['AMQP_URI']}) as rpc:
-        a = rpc.chitchat.predict(q)
+        a = rpc.chitchat.predict(q, id)
 
     result = {
+        'session': id,
         'question': q,
         'answer': a
+    }
+    return jsonify(result), 200
+
+
+@app.route('/init_session', methods=['GET'])
+def init_session():
+    """
+    Init new or reset existing user session
+    ---
+    parameters:
+      - name: session
+        in: query
+        required: true
+        type: string
+    """
+    id = request.args.get('session')
+    with ClusterRpcProxy({'AMQP_URI': os.environ['AMQP_URI']}) as rpc:
+        a = rpc.chitchat.init_session(id)
+
+    result = {
+        'session': id
     }
     return jsonify(result), 200
 
